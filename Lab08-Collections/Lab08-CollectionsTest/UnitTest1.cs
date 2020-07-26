425c497e3b776657fb3eda4a462b182245a93346
Robert Carter <racarter1215@gmail.com>
2020-07-25 22:38:39 -0700

    finished something

--- Lab08-Collections/Lab08-CollectionsTest/UnitTest1.cs
+++ Lab08-Collections/Lab08-CollectionsTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Lab08_Collections;
 using System;
 using Xunit;
 
@@ -6,9 +7,17 @@ namespace Lab08_CollectionsTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void AddBook()
         {
+            //Arrange
+            Library<Book> library = new Library<Book>();
 
+            //Act
+            Book test = new Book() { Title = "1984", Author = new Author() { FirstName = "George", LastName = "Orwell" }, Genre = Genre.Politics };
+
+            library.AddBook(test);
+            //Assert
+            Assert.Equal((System.Collections.Generic.IEnumerable<char>)test, Library.Count());
         }
     }
 }
